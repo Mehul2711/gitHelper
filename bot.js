3477fbaabd16096899ac5a1677db8b6ed2060213
Mehul Kumar <mehul271102@gmail.com>
2024-11-19 11:05:21 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const TelegramBot = require("node-telegram-bot-api");
-const explainCommand = require("./commands"); // Git command logic
+const explainCommand = require("./commands"); 
 
 const token = process.env.TELEGRAM_BOT_TOKEN;
 
